a0b296b6c22bae40447c04974f6dcfa4585eabe0
810096 <59532875+810096@users.noreply.github.com>
2020-03-23 11:51:15 +0800

    Update school4.py

--- school4.py
+++ school4.py
@@ -1,17 +1,17 @@
 import requests, time, json, sys
 from datetime import datetime, timedelta
 
-start = datetime(2020, 3, 17, 0, 0)
+start = datetime(2020, 3, 18, 0, 0)
 
-print("時\tpm2.5")
+print('時\tpm2.5')
 for i in range(24):
-    ftime = start.strftime("%Y%m%d%H%M")
-    filename = "D:\\pm25_"+ftime+".json"
+    ftime = start.strftime('%Y%m%d%H%M')
+    filename = 'D:\\pm25_'+ftime+'.json'
     try:
-        with open(filename, 'r', encoding="UTF-8") as f:
+        with open(filename, 'r', encoding='UTF-8') as f:
             datas = json.loads(f.read())
-            print(start.strftime("%H")+", ", datas[7]['PM25'])
+            print(start.strftime('%H')+'\t'+datas[7]['PM25'])
     except:
-        print(filetime, sys.exe_info()[0], "not found!!")
-        
+        print(filename, sys.exc_info()[0],'not found!!')
+
     start = start + timedelta(hours=1)
